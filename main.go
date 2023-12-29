80de2b7b0f86c04054ec3271ddc11cf3564f1ad1
arshabbir <64956766+arshabbir@users.noreply.github.com>
2023-12-29 15:41:13 +0530

    Replaced all comments in main

--- main.go
+++ main.go
@@ -4,11 +4,9 @@ import "fmt"
 
 func main() {
 
-	//Comment  added in main branch
-	fmt.Println("Addedin feature branch ")
+	
 
-	//Comment added in main branch
-
-	//Comment added in feature 3 branch
+	fmt.Println("Added in origin/master"
+		    
 
 }
